090d58cd501680664995c08d2e5a4a0b06894b79
Roma <Roma@DESKTOP-FRTKI64>
2021-10-03 13:38:44 +0300

    first commit

---
+++ lab-4.3.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    double x, xk, xp, dx, a, b, c, F;
+
+    cout << "xk = "; cin >> xk;
+    cout << "xp = "; cin >> xp;
+    cout << "dx = "; cin >> dx;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+
+    cout << fixed;
+    cout << "----------------------------" << endl;
+    cout << "|" << setw(5) << "x" << "|";
+    cout << setw(7) << "y" << "|" << endl;
+    cout << "----------------------------" << endl;
+
+    x = xp;
+    while (x <= xk)
+    {
+        if (c < 0 && x != 0)
+            F = (-a * x) - c;
+        else
+            if (c > 0 && x == 0)
+                F = (x - a) / (-c);
+            else
+                F = (b * x) / (c - a);
+        cout << "|" << setw(7) << setprecision(2) << F;
+        cout << "  |" << setw(10) << setprecision(3) << x;
+        cout << "    |" << endl;
+        x += dx;
+    } 
+    cout << "----------------------------" << endl;
+
+    return 0;
+}
