430f4b102593aac37ab7b1a68275b675ce234f73
Roma <Roma@DESKTOP-FRTKI64>
2021-10-07 01:43:30 +0300

    third commit

--- lab-4.3.cpp
+++ lab-4.3.cpp
@@ -8,32 +8,32 @@ int main()
 {
     double x, xk, xp, dx, a, b, c, F;
 
-    cout << "xk = "; cin >> xk;
     cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
     cout << "dx = "; cin >> dx;
     cout << "a = "; cin >> a;
     cout << "b = "; cin >> b;
     cout << "c = "; cin >> c;
 
     cout << fixed;
-    cout << "----------------------------" << endl;
-    cout << "|" << setw(5) << "x" << "|";
-    cout << setw(7) << "F" << "|" << endl;
-    cout << "----------------------------" << endl;
+    cout << "---------------------------" << endl;
+    cout << "|" << setw(5) << "x" << " |"
+        << setw(7) << "F" << " |" << endl;
+    cout << "---------------------------" << endl;
 
     x = xp;
     while (x <= xk)
     {
         if (c < 0 && x != 0)
             F = (-a * x) - c;
-        else
-            if (c > 0 && x == 0)
-                F = (x - a) / (-c);
-            else
-                F = (b * x) / (c - a);
-        cout << "|" << setw(7) << setprecision(2) << F;
-        cout << "  |" << setw(10) << setprecision(3) << x;
-        cout << "    |" << endl;
+        if (c > 0 && x == 0)
+            F = (x - a) / (-c);
+        if (!(c < 0 && x != 0) && !(c > 0 && x == 0))
+            F = (b * x) / (c - a);
+
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << F
+            << " |" << endl;
         x += dx;
     } 
     cout << "----------------------------" << endl;
